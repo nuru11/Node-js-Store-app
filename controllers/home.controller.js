c74bbc9df6ab0080dd24840d97ea773fee6ce9b3
nuru ibrahim <nuruibrahim11111@gmail.com>
2022-12-09 06:08:02 -0800

    test

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,26 +1,21 @@
 const homeModel = require('../models/home.model');
 
-/*exports.getHome = (req,res,next) => {
- 
-   console.log(req.flash("postCartValidationError"))
-
-    let category = req.query.category
-    let validCategory = ['clothes', 'technology',]
-    let productsPromise;
-    if(category && validCategory.includes(category)) productsPromise =  homeModel.getProductsByCategory(category)
-     else productsPromise = homeModel.getAllProduct()
-     productsPromise.then(products=>{
-      res.render("index", {
-         products: products,
-         isUser: req.session.userId, 
-         isAdmin: req.session.isAdmin,
-         postCartValidationError: req.flash("postCartValidationError")[0],
-         pageTitle: "Home"
-      })
-     }) 
-   
-}*/
-
 exports.getHome = (req, res, next) => {
-  res.send('hello nuru');
+  console.log(req.flash('postCartValidationError'));
+
+  let category = req.query.category;
+  let validCategory = ['clothes', 'technology'];
+  let productsPromise;
+  if (category && validCategory.includes(category))
+    productsPromise = homeModel.getProductsByCategory(category);
+  else productsPromise = homeModel.getAllProduct();
+  productsPromise.then((products) => {
+    res.render('index', {
+      products: products,
+      isUser: req.session.userId,
+      isAdmin: req.session.isAdmin,
+      postCartValidationError: req.flash('postCartValidationError')[0],
+      pageTitle: 'Home',
+    });
+  });
 };
