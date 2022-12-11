50c827ae3ddc066a4f597c721cbcfc86d867dc01
nuru ibrahim <nuruibrahim11111@gmail.com>
2022-12-10 22:45:01 -0800

    t

--- index.js
+++ index.js
@@ -1,65 +1,63 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-  }
+  require('dotenv').config();
+}
 
-const express = require("express")
-const app = express()
+const express = require('express');
+const app = express();
 
-const path = require("path")
+const path = require('path');
 
-const flash = require("connect-flash")
+const flash = require('connect-flash');
 
-const session = require("express-session")
-const SessionStore = require("connect-mongodb-session")(session)
+const session = require('express-session');
+const SessionStore = require('connect-mongodb-session')(session);
 
-const homeRouter = require("./routes/home.route")
-const productRouter = require("./routes/product.route")
-const authRouter = require('./routes/auth.route')
-const cartRouter = require("./routes/cart.route")
-const adminRouter = require("./routes/admin.route")
+const homeRouter = require('./routes/home.route');
+const productRouter = require('./routes/product.route');
+const authRouter = require('./routes/auth.route');
+const cartRouter = require('./routes/cart.route');
+const adminRouter = require('./routes/admin.route');
 
-app.use(express.static(path.join(__dirname, "assets")))
-app.use(express.static(path.join(__dirname, "images")))
-app.use(flash())
-
+app.use(express.static(path.join(__dirname, 'assets')));
+app.use(express.static(path.join(__dirname, 'images')));
+app.use(flash());
 
 const STORE = new SessionStore({
-    uri: 'mongodb+srv://nuru11:0966202667@cluster0.f92keez.mongodb.net/online-shop?retryWrites=true&w=majority',
-    collection: "session"
-})
+  uri: 'mongodb+srv://nuru11:0966202667@cluster0.f92keez.mongodb.net/online-shop?retryWrites=true&w=majority',
+  collection: 'session',
+});
 
-
-app.use(session({
-    secret: "this is my secret to hash express session....",
+app.use(
+  session({
+    secret: 'this is my secret to hash express session....',
     saveUninitialized: false,
-    cookie:{
-        maxAge: 1*60*60*100
+    cookie: {
+      maxAge: 1 * 60 * 60 * 100,
     },
-    store: STORE
-}))
+    store: STORE,
+  })
+);
 
-app.set("view engine", "ejs")
-app.set("views", "views") 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-app.use("/", homeRouter)
-app.use("/product", productRouter)
-app.use("/", authRouter)
-app.use("/cart", cartRouter)
-app.use("/admin", adminRouter)
+app.use('/', homeRouter);
+app.use('/product', productRouter);
+app.use('/', authRouter);
+app.use('/cart', cartRouter);
+app.use('/admin', adminRouter);
 
+app.use((req, res, next) => {
+  res.status(404);
+  res.render('not-found', {
+    isUser: req.session.isUser,
+    isAdmin: req.session.isAdmin,
+    pageTitle: 'page not found',
+  });
+});
 
-
-app.use((req, res, next)=> {
-    res.status(404)
-    res.render("not-found", {
-        isUser: req.session.isUser,
-        isAdmin: req.session.isAdmin,
-        pageTitle: "page not found"
-    })
-})
-
- 
-const port = process.env.PORT || 3000
-app.listen(port, ()=>{
-console.log("server listening on port 3000...")
-})
+console.log('heloo nuru');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log('server listening on port 3000...');
+});
